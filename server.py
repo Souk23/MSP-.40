73aab13bab23a2c5f518df9cb96f96ba2814f6d6
Souk23 <123994421+Souk23@users.noreply.github.com>
2024-06-28 22:07:08 +0000

    Mundo guardado 2024-06-28

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-# Versiones disponibles
+11# Versiones disponibles
 # forge, mohist, fabric, vanilla, paper
 
 # Puedes instalar mohist después de instalar forge desde el menú de gestionar
@@ -16,13 +16,6 @@
 # sa		      Sudamérica (São Paulo)
 # us		      Estados unidos (Ohio)
 # us-cal-1	      Estados unidos (California)
-
-
-
-
-
-
-
 
 
 
